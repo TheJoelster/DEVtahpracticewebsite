3994477fd579612aee7b3c4031aaba3b50bd9738
Joel <jocr2024@outlook.com>
2022-11-29 12:50:32 -0800

    logic fix

--- practice/quizlogic.js
+++ practice/quizlogic.js
@@ -126,10 +126,10 @@ fetch(directQ)
         completedQuestions[n] = i;
         console.log("Questionbank: " + completedQuestions[n]);
         questionDisplay.textContent = dataQA[i]["qu"]
-        o1.innerHTML = dataQA[i]["a1"]
-        o2.innerHTML = dataQA[i]["a2"]
-        o3.innerHTML = dataQA[i]["a3"]
-        o4.innerHTML = dataQA[i]["a4"]
+        o1.innerHTML = dataQA[i]["a1"] + '<input type="radio" name="radio" value="1"><span class="checkmark"></span>'
+        o2.innerHTML = dataQA[i]["a2"] + '<input type="radio" name="radio" value="2"><span class="checkmark"></span>'
+        o3.innerHTML = dataQA[i]["a3"] + '<input type="radio" name="radio" value="3"><span class="checkmark"></span>'
+        o4.innerHTML = dataQA[i]["a4"] + '<input type="radio" name="radio" value="4"><span class="checkmark"></span>'
         ansValue = dataQA[i]["ans"]
         speedUpper = dataQA[i]["timeUpper"]
         speedMidRange = dataQA[i]["timeMid"]
